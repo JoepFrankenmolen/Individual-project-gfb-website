c645f216af5c87ad170305ba237d5b6d1ca09772
Frankenmolen,Joep Frankenmolen J <joep.frankenmolen@gmail.com>
2021-11-01 07:44:39 +0100

    fixed some bugs

--- src/components/Admin/Group/GroupSearchBar.js
+++ src/components/Admin/Group/GroupSearchBar.js
@@ -2,27 +2,11 @@
 //this wil for example be a general thing
 
 const GroupSearchBar = () =>{
-    const [inputText, setInputText] = useState({
-        title: "",
-    })
 
-    const onChange = e => {
-        setInputText({
-        ...inputText,
-        [e.target.name]: e.target.value,
-        })
-    }
 
     return(
     <div className="form-container">
-        <input
-            type="text"
-            className="input-text"
-            placeholder="Add todo..."
-            value={inputText.title}
-            name="title"
-            onChange={onChange}
-        />
+        
     </div>    
     )
 }
