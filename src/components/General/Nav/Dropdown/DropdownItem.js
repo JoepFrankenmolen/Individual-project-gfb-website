ffa56834940b6724d49d2b67415047daf789d914
Frankenmolen,Joep Frankenmolen J <j.frankenmolen@student.fontys.nl>
2021-12-15 12:22:04 +0100

    fixed the size of dropwown

--- src/components/General/Nav/Dropdown/DropdownItem.js
+++ src/components/General/Nav/Dropdown/DropdownItem.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import {useHistory} from "react-router-dom"
 import {FaCaretDown} from "react-icons/fa"
 
 const DropdownItem = props => 
 {
     const history = useHistory();
-    
+    const[Width, setWidth] = useState(200)
+
     const redirect = (link) =>
     {
         history.push(link); 
     }
 
+    useEffect(() => {
+        var temp =  document.getElementById(props.item.id).clientWidth
+        console.log(temp)
+        temp = temp + 50
+        console.log(temp)
+        setWidth(temp)
+        // console.log(width)
+        // console.log(document.getElementById(props.item.id).clientWidth)
+      }, []);
+
 
     return (
-        <div className='dropdown-item'>
-            <div class="dropdown-button" onClick={() => redirect(props.item.route)}>
+        <div id={props.item.id} className='dropdown-item'>
+            <div class="dropdown-button"  onClick={() => redirect(props.item.route)}>
                 <a className='dropdown-content-text dropdown-content-text-button'>{props.item.title}</a>
                 <FaCaretDown className='dropdown-caret' />
             </div>
-            <div class="dropdown-content">
+            <div className="dropdown-content" style={{width: Width }}>
                 <a onClick={() => redirect(props.item.route)} className='dropdown-content-text dropdown-content-text-down'>{props.item.title}</a>
                 {props.item.subPages.map((page)=>(
                     <a  className='dropdown-content-text dropdown-content-text-down' onClick={() => redirect(page.route)}>{page.title}</a>
